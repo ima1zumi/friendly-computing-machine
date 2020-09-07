c00f00aca4d5a6125a5e71086dc624e638cc6291
ima1zumi <tesioa@gmail.com>
2020-09-08 00:37:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# participation-award
+Shows the number of times you participated in the event on connpass.
+
+## install
+```sh
+$ npm install participation_award
+```
+## Usage
+``` sh
+$ ./node_modules/participation_award
+connpassのユーザーネームを入力してください。
+ima1zumi # <Ctrl-D>
+ima1zumiさんのイベント参加合計回数は34回です。
+```
+
+``` sh
+$ ./node_modules/participation_award -y 2020 -m 09
+connpassのユーザーネームを入力してください。
+ima1zumi # <Ctrl-D>
+ima1zumiさんは2020年9月のイベントに3回参加予定です。
+```
+
+## License
+This software is released under the MIT License, see LICENSE.txt.
---
+++ main.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch')
+const argv = require('minimist')(process.argv.slice(2))
+
+async function countParticipation (name) {
+  const response = await fetch(`https://connpass.com/api/v1/event/?nickname=${name}`)
+  const json = await response.json()
+  console.log(`${name}さんのイベント参加合計回数は${json.results_available}回です。`)
+}
+
+async function countParticipationInYearsAndMonths (name, y, m) {
+  const response = await fetch(`https://connpass.com/api/v1/event/?nickname=${name}&ym=${y}${m}`)
+  const json = await response.json()
+  console.log(`${name}さんは${y}年${m}月のイベントに${json.results_available}回参加予定です。`)
+}
+
+function exec () {
+  console.log('connpassのユーザーネームを入力してください。')
+  process.stdin.resume()
+  process.stdin.setEncoding('utf8')
+  let inputString = ''
+
+  process.stdin.on('data', function (chunk) {
+    inputString += chunk
+  })
+
+  process.stdin.on('end', function () {
+    const name = inputString.split('\n')[0]
+    if (argv.y && argv.m) {
+      countParticipationInYearsAndMonths(name, argv.y, argv.m)
+    } else {
+      countParticipation(name)
+    }
+  })
+}
+
+exec()
+
